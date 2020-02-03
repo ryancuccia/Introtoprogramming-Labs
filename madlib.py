208c590a8301adc004b2a8da396c01e1747c63b2
Ryan Cuccia <ryan.cuccia1@marist.edu>
2020-02-03 18:01:43 -0500

    solution to Mad Libs exercise.

---
+++ madlib.py
@@ -0,0 +1,5 @@
+noun = input("Enter a Noun: ")
+verb = input("Enter a Verb: ")
+place = input("Enter a Place: ")
+food = input("Enter a food: ")
+print(noun + " likes to " + verb + food + " in the " + place)
