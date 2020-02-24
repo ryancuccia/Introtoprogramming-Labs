fd56a62df446054f4b4cb69eb42509f1b3f5871e
Ryan Cuccia <ryan.cuccia1@marist.edu>
2020-02-24 18:16:31 -0500

    Sample lab code

---
+++ credentials.py
@@ -0,0 +1,13 @@
+def main():
+    def operator():
+        first = input("Enter your first name: ")
+        last = input("Enter your last name: ")
+        username = first + last
+        password = input("Create a new password: ")
+        while False:
+            print("Fool of a Took! That password is feeble!")
+            password = input("Create a new password: ")
+        print("The force is strong with this one,,,")
+        print("Your email address is", username + "@marist.edu")
+    operator()
+main()
