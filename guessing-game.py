28c25473fe17667a0e61de42e4172a2a59ea2ce0
Ryan Cuccia <ryan.cuccia1@marist.edu>
2020-02-24 17:44:33 -0500

    More usablity improvements to the guessing game:

--- guessing-game.py
+++ guessing-game.py
@@ -1,13 +1,24 @@
 def main():
     print("Welcome to THE Python Guessing Game.")
     animal = "wolf"
-    user_animal = input("Please try to guess the correct animal: ")
-    butt = input("Do you want to try again? ")
-    while True:
-        if user_animal != animal:
-            user_animal = input("Try again. Please try to guess the correct animal: ")
+    zero = 0
+    while zero == 0:
+        guess = input("Please try to guess the right answer: ")
+        if guess.lower()== animal:
+            zero += 1
+            print("You're right!")
+            wolfgang = 0
+            while wolfgang == 0:
+                response = input("Do you think this animal is cool? :")
+                if response[0].lower() == "n":
+                    print("Watch yourself wolves can eat you.")
+                    wolfgang += 1
+                elif response[0].lower() == "y":
+                    print("Nice.")
+                    wolfgang += 1
+        elif guess[0].lower() == "q":
+            zero += 1
         else:
-            print("Congradualtions! You win!!")
-            break
-
+            print("Wrong animal, try again.")
+    print("THE Python Guessing Game is over, good bye.")
 main()
