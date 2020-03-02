4eb3dae49f01a2b14570f66a4f7fccca82634f04
Ryan Cuccia <ryan.cuccia1@marist.edu>
2020-03-02 17:50:15 -0500

    Finished the requirements for lab 05

--- credentials.py
+++ credentials.py
@@ -2,13 +2,20 @@ def main():
     def nameget():
         first = input("Enter your first name: ")
         last = input("Enter your last name: ")
+        first = first.lower()
+        last = last.lower()
         return(first, last)
     def namebuild(first, last):
         return(first + "." + last)
+    def strengthcheck2(password):
+        while password.islower() or password.isupper():
+            print("Password must contain atleast one uppercase and one lowercase character.")
+            password = input("Pick a new password: ")
     def strengthcheck(password):
         while len(password)<8:
             print("Password too short.")
             password = input("Pick a new password: ")
+        strengthcheck2(password)
         print("Valid password.")
         return password
     def operator():
